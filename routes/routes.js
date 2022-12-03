69aa10a82cf157754f6a7838088bcf1b0d040433
mrjakeschultz <111388769+mrjakeschultz@users.noreply.github.com>
2022-12-02 20:36:52 -0600

    scaffold new routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,19 @@
-const fse = require("fse");
+const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const router = express.Router();
+
+router.get("/api/notes/", (req, res) => {
+	//send note json
+});
+
+router.delete("/api/notes/:id", (req, res) => {
+	//filter through array and remove
+});
+
+router.post("/api/notes", (req, res) => {
+	//write body of note
+	//need/get guid for id
+});
+
+module.exports = router;
